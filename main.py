17539a366315842e1eb5dd1ebbf544716a71374b
abdullah-man <am0776137@gmail.com>
2023-12-11 15:05:18 +0500

    GUI : Ask Query section now appearing ;

--- main.py
+++ main.py
@@ -43,12 +43,15 @@ all_data_frames = list()
 error_sheet_info = dict()
 
 # Setting API_KEY_SET as session variables to be accessed and set globally in streamlit application 
-if ('API_KEY_SET' and 'API_BOOL') not in st.session_state: 
+if ('API_KEY_SET' and 'API_BOOL' and 'Uploaded' and 'ALL_DF') not in st.session_state: 
     st.session_state['API_KEY_SET'] = ""
     st.session_state['API_BOOL'] = False
+    st.session_state['Uploaded'] = False
+    st.session_state['ALL_DF'] = list()
 
 
-# --------------------- SECTION : Set API Key --------------------- 
+# --------------------- SECTION : Set API Key ---------------------
+
 if (selected_section == "Set API Key"):
     
     # Case : if API key already set
@@ -59,10 +62,11 @@ if (selected_section == "Set API Key"):
     # Case : if API key not already set
     if st.button("Submit"):
         # if a value is entered as API key and it starts with sk- then:
-        if (api_key) and api_key.startswith('sk-'): 
-            st.session_state['API_KEY_SET'] = api_key
-            st.session_state['API_BOOL'] = True
-            st.success(f"API Key set successfully as {st.session_state['API_KEY_SET']}")
+        if api_key:
+            if api_key.startswith('sk-'): 
+                st.session_state['API_KEY_SET'] = api_key
+                st.session_state['API_BOOL'] = True
+                st.success(f"API Key set successfully as {st.session_state['API_KEY_SET']}")
         
         # if it is empty or does not start with sk- then:
         else:
@@ -71,88 +75,81 @@ if (selected_section == "Set API Key"):
     
     
 # --------------------- SECTION : Upload Files ---------------------
-if selected_section == "Upload Files":
-    # st.header("Upload Files")
+
+# if API Key already set then open this section
+if (selected_section == "Upload Files") and (st.session_state['API_BOOL'] is True):     
     uploaded_files = st.file_uploader("Select multiple files:", type=["xlsx"], accept_multiple_files=True)
     
-    if uploaded_files:
+    if uploaded_files: # if uploaded_files contain any file then
         st.write("Selected Files:")
         for file in uploaded_files:
             st.write(file.name)
 
-        if st.button("Upload"):
+        
+        if st.button("Upload"): # if Upload button is clicked
             with st.spinner("Uploading files..."):
-                # Simulate file upload process (replace this with actual file processing logic)
                 uploaded_files_names = [file.name for file in uploaded_files]
-                uploaded_files_paths = [file.getvalue() for file in uploaded_files]
+                # uploaded_files_paths = [file.getvalue() for file in uploaded_files]
                 
-                # Loading Excel files as Pandas dataframe objects
-                for file in uploaded_files_paths:
-                    load_response = load_excel(excel_file=file)
-                    # case 1 : successfull file load
-                    if isinstance(load_response, dict):
-                        sheets = load_response['dataframes']
-                        all_data_frames = all_data_frames + sheets
+            # --- Loading Excel files as Pandas dataframe objects ---
 
-                        # error_sheets = load_response['error_sheets']
-                        # error_sheet_info[file] = error_sheets
-                    
-                    # case 2 : failed file load
-                    if load_response is None:
-                        st.write(f"File {file} could not be opened.")
-
-        st.write("Selected Files:")
-        for file in uploaded_files:
-            st.write(file.name)
-
-        if st.button("Upload"):
-            with st.spinner("Uploading files..."):
-                # Simulate file upload process (replace this with actual file processing logic)
-                uploaded_files_names = [file.name for file in uploaded_files]
-                uploaded_files_paths = [file.getvalue() for file in uploaded_files]
+            for file in uploaded_files: 
                 
-                # Loading Excel files as Pandas dataframe objects
-                for file in uploaded_files_paths:
-                    load_response = load_excel(excel_file=file)
-                    # case 1 : successfull file load
-                    if isinstance(load_response, dict):
-                        sheets = load_response['dataframes']
-                        all_data_frames = all_data_frames + sheets
+                load_response = load_excel(excel_file=file)
 
-                        # error_sheets = load_response['error_sheets']
-                        # error_sheet_info[file] = error_sheets
+                # case 1 : successfull file load
+                if isinstance(load_response, dict):
+                    sheets = load_response['dataframes']
+                    st.session_state['ALL_DF'] = st.session_state['ALL_DF'] + sheets
+
+                    # Handling Error Sheets
+                    # error_sheets = load_response['error_sheets']
+                    # error_sheet_info[file] = error_sheets
                     
-                    # case 2 : failed file load
-                    if load_response is None:
-                        st.write(f"File {file} could not be opened.")
+                    st.success(f"File {file.name} successfully opened.")
+                
+                # case 2 : failed to load the entire file
+                if load_response is None:
+                    st.error(f"File {file.name} could not be opened.")
 
-                # st.write(f"These files have these problamatic sheets.")
-                # st.write(list(error_sheet_info.items()))
     
-
-            st.success("Files uploaded successfully!")
-            st.write("Successfully uploaded files:")
-            st.write(uploaded_files_names)
+    # Setting Uploaded to True if any dataframe was added to st.session_state['ALL_DF'] varibale
+    # This means now we can perform queries on this dataframe
+    if len(st.session_state['ALL_DF']) > 0: # if this list contain any dataframe then:
+        st.session_state['Uploaded'] = True
+        st.success("Files Uploaded and Opened Successfully!")
+        
+        # displaying data for checking
+        for df in st.session_state['ALL_DF']:
+            st.dataframe(df, use_container_width=True)
 
 
 # --------------------- SECTION : Ask Queries / Chat ---------------------
-if selected_section == "Ask":
-    # st.header("Chat Section")
-    user_input = st.text_input("Type your message:")
-    chat_messages = st.empty()  # Create an empty container for chat messages
 
-    if st.button("Submit"):
-        if user_input:
-            # Process user input and generate a response (replace with your logic)
-            response = "You said: " + user_input
+# if Files already uploaded then open this section
+if (selected_section == "Ask Query"): 
+    input_text = st.text_area("Enter the query")
+    
+    # Ask questions / chat
+    if input_text:
+        if st.button("Chat with data"):
+            st.info("Your Query: "+ input_text)
+            st.success("This is reply.")
 
-            # Append user's message and response to chat_messages
-            chat_messages.text(f"You: {user_input}")
-            chat_messages.text(f"Bot: {response}")
-        else:
-            st.warning("Please enter a message before submitting.")
-
-
+    
+    # api_key = st.text_input("Enter API Key:")
+    # # Case : if API key not already set
+    # if st.button("Submit"):
+    #     # if a value is entered as API key and it starts with sk- then:
+    #     if (api_key) and api_key.startswith('sk-'): 
+    #         st.session_state['API_KEY_SET'] = api_key
+    #         st.session_state['API_BOOL'] = True
+    #         st.success(f"API Key set successfully as {st.session_state['API_KEY_SET']}")
+        
+    #     # if it is empty or does not start with sk- then:
+    #     else:
+    #         st.error("Please enter a valid API Key.")
+    #         st.error(f"Please try again. You entered: {api_key}")
 
 # To Run:
 # streamlit run main.py --server.maxUploadSize 1000
